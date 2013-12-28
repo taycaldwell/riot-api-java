3043e62c662d6cb19606581d0772f416f50524f7
Taylor <taylor@linux-hlv4.(none)>
2013-12-27 22:44:25 -0700

    Updating with documentation

--- src/main/java/riotapi/RiotAPI.java
+++ src/main/java/riotapi/RiotAPI.java
@@ -13,27 +13,42 @@ import dto.*;
 
 
 public class RiotAPI {
-
-	private String key = "YOUR API KEY";
+	
+	/**The base URL for all API requests*/
 	private String baseURL = "https://prod.api.pvp.net/api/lol/";
+	
+	/**Your personal API key*/
+	private String key = "YOUR API KEY";
+	
+	/**The desired region*/
 	private String region = ""; 
+	
+	/**The desired season*/
 	private String season = "";
 	
+	/**Constructor*/
 	public RiotAPI() {
 		
 	}
 	
+	/**Constructor*/
 	public RiotAPI(String key) {
 		
 		this.setKey(key);
 	}
 	
+	/**Constructor*/
 	public RiotAPI(String key, String region) {
 		
 		this.setKey(key);
 		this.setRegion(region);
 	}
 	
+	/**
+	 * Get all the champions for a set region
+	 * @return A list of all the champions for the set region
+	 * @see ChampionList
+	 */
 	public ChampionList getChampions() {
 		
 		String url = getBaseURL() + getRegion() + "/v1.1/champion?api_key=" + getKey();
@@ -51,6 +66,12 @@ public class RiotAPI {
 	    return championList;
 	}
 	
+	/**
+	 * Get all the champions for a given region
+	 * @param region The desired region
+	 * @return A list of all the champions for the set region
+	 * @see ChampionList
+	 */
 	public ChampionList getChampions(String region) {
 	
 		String url = getBaseURL() + region + "/v1.1/champion?api_key=" + getKey();
@@ -68,6 +89,15 @@ public class RiotAPI {
 	    return championList;
 	}
     
+	/**
+	 * Get all the champions for a set region
+	 * @param freeToPlay Only free to play
+	 * @return 
+	 * A list of all the free to play champions for the set region when freeToPlay is true
+	 * A list of all the champions for the set region when freeToPlay is false
+	 * @see ChampionList
+	 * 
+	 */
 	public ChampionList getChampions(boolean freeToPlay) {
 
 		String url = getBaseURL() + getRegion() + "/v1.1/champion?freeToPlay=" + freeToPlay + "&api_key=" + getKey();
@@ -85,6 +115,15 @@ public class RiotAPI {
 	    return championList;
 	}
 	
+	/**
+	 * Get all champions for a given region
+	 * @param region The desired region
+	 * @param freeToPlay Only free to play
+	 * @return
+	 * A list of all the free to play champions for the given region when freeToPlay is true
+	 * A list of all the champions for the given region when freeToPlay is false
+	 * @see ChampionList
+	 */
 	public ChampionList getChampions(String region, boolean freeToPlay) {
 		
 		String url = getBaseURL() + region + "/v1.1/champion?freeToPlay=" + freeToPlay + "&api_key=" + getKey();
@@ -102,6 +141,12 @@ public class RiotAPI {
 	    return championList;
 	}
 	
+	/**
+	 * Get all free to play champions for a given region
+	 * @param region The desired region
+	 * @return A list of all the free to play champions for the given region
+	 * @see ChampionList
+	 */
 	public ChampionList getFreeToPlayChampions(String region) {
 
 		String url = getBaseURL() + region + "/v1.1/champion?freeToPlay=true" + "&api_key=" + getKey();
@@ -119,6 +164,11 @@ public class RiotAPI {
 	    return championList;
 	}
 	
+	/**
+	 * Get all free to play champions for a set region
+	 * @return A list of all the free to play champions for the set region
+	 * @see ChampionList
+	 */
 	public ChampionList getFreeToPlayChampions() {
 
 		String url = getBaseURL() + getRegion() + "/v1.1/champion?freeToPlay=true" + "&api_key=" + getKey();
@@ -136,6 +186,13 @@ public class RiotAPI {
 	    return championList;
 	}
 	
+	/**
+	 * Get recent games for a given summoner
+	 * @param region The desired region
+	 * @param summonerId The ID of the desired summoner
+	 * @return Recent games of the given summoner
+	 * @see RecentGames
+	 */
 	public RecentGames getRecentGames(String region, long summonerId) {
 		
 		String url = getBaseURL() + region + "/v1.2/game/by-summoner/" + summonerId + "/recent?api_key=" + getKey();
@@ -153,6 +210,12 @@ public class RiotAPI {
 	    return recentGames;
 	}
 	
+	/**
+	 * Get recent games for a given summoner
+	 * @param summonerId The ID of the desired summoner
+	 * @return Recent games of the given summoner
+	 * @see RecentGames
+	 */
 	public RecentGames getRecentGames(long summonerId) {
 		
 		String url = getBaseURL() + getRegion() + "/v1.2/game/by-summoner/" + summonerId + "/recent?api_key=" + getKey();
@@ -169,7 +232,14 @@ public class RiotAPI {
 	    
 	    return recentGames;
 	}
-
+	
+	/**
+	 * Get leagues of a given summoner
+	 * @param region The desired region
+	 * @param summonerId The ID of the desired summoner
+	 * @return A map of leagues the given summoner belongs to
+	 * @see League
+	 */
 	public Map<String, League> getLeagues(String region, long summonerId) {
 		
 		String url = getBaseURL() + region + "/v2.2/league/by-summoner/" + summonerId + "?api_key=" + getKey();
@@ -187,6 +257,12 @@ public class RiotAPI {
 	    return leagues;
 	}
 	
+	/**
+	 * Get leagues of a given summoner
+	 * @param summonerId The ID of the desired summoner
+	 * @return A map of leagues the given summoner belongs to
+	 * @see League
+	 */
 	public Map<String, League> getLeagues(long summonerId) {
 		
 		String url = getBaseURL() + getRegion() + "/v2.2/league/by-summoner/" + summonerId + "?api_key=" + getKey();
@@ -204,6 +280,14 @@ public class RiotAPI {
 	    return leagues;
 	}
 	
+	/**
+	 * Get a summary of player statistics for a given summoner
+	 * @param region The desired region
+	 * @param summonerId The ID of the desired summoner
+	 * @param season The desired season
+	 * @return A summary of player statistics for the given summoner
+	 * @see PlayerStatsSummaryList
+	 */
 	public PlayerStatsSummaryList getPlayerStatsSummary(String region, long summonerId, String season) {
 		
 		String url = getBaseURL() + region + "/v1.2/stats/by-summoner/" + summonerId + "/summary?season=" + season + "&api_key=" + getKey();
@@ -221,6 +305,13 @@ public class RiotAPI {
 	    return summaryList;
 	}
 	
+	/**
+	 * Get a summary of player statistics for a given summoner
+	 * @param summonerId The ID of the desired summoner
+	 * @param season The desired season
+	 * @return A summary of player statistics for the given summoner
+	 * @see PlayerStatsSummaryList
+	 */
 	public PlayerStatsSummaryList getPlayerStatsSummary(long summonerId, String season) {
 		
 		String url = getBaseURL() + getRegion() + "/v1.2/stats/by-summoner/" + summonerId + "/summary?season=" + season + "&api_key=" + getKey();
@@ -238,6 +329,13 @@ public class RiotAPI {
 	    return summaryList;
 	}
 	
+	/**
+	 * Get a summary of player statistics for a given summoner
+	 * @param region The desired region
+	 * @param summonerId The ID of the desired summoner
+	 * @return A summary of player statistics for the given summoner
+	 * @see PlayerStatsSummaryList
+	 */
 	public PlayerStatsSummaryList getPlayerStatsSummary(String region, long summonerId) {
 		
 		String url = getBaseURL() + region + "/v1.2/stats/by-summoner/" + summonerId + "/summary?season=" + getSeason() + "&api_key=" + getKey();
@@ -255,6 +353,12 @@ public class RiotAPI {
 	    return summaryList;
 	}
 	
+	/**
+	 * Get a summary of player statistics for a given summoner
+	 * @param summonerId The ID of the desired summoner
+	 * @return A summary of player statistics for the given summoner
+	 * @see PlayerStatsSummaryList
+	 */
 	public PlayerStatsSummaryList getPlayerStatsSummary(long summonerId) {
 		
 		String url = getBaseURL() + getRegion() + "/v1.2/stats/by-summoner/" + summonerId + "/summary?season=" + getSeason() + "&api_key=" + getKey();
@@ -272,6 +376,14 @@ public class RiotAPI {
 	    return summaryList;
 	}
 	
+	/**
+	 * Get the ranked statistics of a given summoner
+	 * @param region The desired region
+	 * @param summonerId The ID of the desired summoner
+	 * @param season The desired season
+	 * @return Ranked statistics of the given summoner
+	 * @see RankedStats
+	 */
 	public RankedStats getRankedStats(String region, long summonerId, String season) {
 		
 		String url = getBaseURL() + region + "/v1.2/stats/by-summoner/" + summonerId + "/ranked?season=" + season + "&api_key=" + getKey();
@@ -289,7 +401,13 @@ public class RiotAPI {
 	    return rankedStats;
 	}
 	
-	
+	/**
+	 * Get the ranked statistics of a given summoner
+	 * @param summonerId The ID of the desired summoner
+	 * @param season The desired season
+	 * @return Ranked statistics of the given summoner
+	 * @see RankedStats
+	 */
 	public RankedStats getRankedStats(long summonerId, String season) {
 		
 		String url = getBaseURL() + getRegion() + "/v1.2/stats/by-summoner/" + summonerId + "/ranked?season=" + season + "&api_key=" + getKey();
@@ -307,7 +425,13 @@ public class RiotAPI {
 	    return rankedStats;
 	}
 	
-	
+	/**
+	 * Get the ranked statistics of a given summoner
+	 * @param region The desired region
+	 * @param summonerId The ID of the desired summoner
+	 * @return Ranked statistics of the given summoner
+	 * @see RankedStats
+	 */
 	public RankedStats getRankedStats(String region, long summonerId) {
 		
 		String url = getBaseURL() + region + "/v1.2/stats/by-summoner/" + summonerId + "/ranked?season=" + getSeason() + "&api_key=" + getKey();
@@ -325,6 +449,12 @@ public class RiotAPI {
 	    return rankedStats;
 	}
 	
+	/**
+	 * Get the ranked statistics of a given summoner
+	 * @param summonerId The ID of the desired summoner
+	 * @return Ranked statistics of the given summoner
+	 * @see RankedStats
+	 */
 	public RankedStats getRankedStats(long summonerId) {
 		
 		String url = getBaseURL() + getRegion() + "/v1.2/stats/by-summoner/" + summonerId + "/ranked?season=" + getSeason() + "&api_key=" + getKey();
@@ -342,6 +472,13 @@ public class RiotAPI {
 	    return rankedStats;
 	}
 	
+	/**
+	 * Get the mastery pages of a given summoner
+	 * @param region The desired region
+	 * @param summonerId The ID of the desired summoner
+	 * @return Mastery pages of the given summoner
+	 * @see MasteryPages
+	 */
 	public MasteryPages getMasteryPages(String region, long summonerId) {
 		
 		String url = getBaseURL() + region + "/v1.2/summoner/" + summonerId + "/masteries?api_key=" + getKey();
@@ -359,6 +496,12 @@ public class RiotAPI {
 	    return masteryPages;
 	}
 	
+	/**
+	 * Get the mastery pages of a given summoner
+	 * @param summonerId The ID of the desired summoner
+	 * @return Mastery pages of the given summoner
+	 * @see MasteryPages
+	 */
 	public MasteryPages getMasteryPages(long summonerId) {
 		
 		String url = getBaseURL() + getRegion() + "/v1.2/summoner/" + summonerId + "/masteries?api_key=" + getKey();
@@ -376,6 +519,13 @@ public class RiotAPI {
 	    return masteryPages;
 	}
 	
+	/**
+	 * Get the rune pages of a given summoner
+	 * @param region The desired region
+	 * @param summonerId The ID of the desired summoner
+	 * @return Rune pages of the given summoner
+	 * @see RunePages
+	 */
 	public RunePages getRunePages(String region, long summonerId) {
 		
 		String url = getBaseURL() + region + "/v1.2/summoner/" + summonerId + "/runes?api_key=" + getKey();
@@ -393,6 +543,12 @@ public class RiotAPI {
 	    return runePages;
 	}
 	
+	/**
+	 * Get the rune pages of a given summoner
+	 * @param summonerId The ID of the desired summoner
+	 * @return Rune pages of the given summoner
+	 * @see RunePages
+	 */
 	public RunePages getRunePages(long summonerId) {
 		
 		String url = getBaseURL() + getRegion() + "/v1.2/summoner/" + summonerId + "/runes?api_key=" + getKey();
@@ -410,6 +566,13 @@ public class RiotAPI {
 	    return runePages;
 	}
 	
+	/**
+	 * Get a summoner by name
+	 * @param region The desired region
+	 * @param name The name of the desired summoner
+	 * @return The desired summoner
+	 * @see Summoner
+	 */
 	public Summoner getSummoner(String region, String name) {
 		
 		name = name.replaceAll("\\s+", "");
@@ -428,6 +591,12 @@ public class RiotAPI {
 	    return summoner;
 	}
 	
+	/**
+	 * Get a summoner by name
+	 * @param name The name of the desired summoner
+	 * @return The desired summoner
+	 * @see Summoner
+	 */
 	public Summoner getSummoner(String name) {
 		
 		name = name.replaceAll("\\s+", "");
@@ -446,6 +615,13 @@ public class RiotAPI {
 	    return summoner;
 	}
 	
+	/**
+	 * Get a summoner by ID
+	 * @param region The desired region
+	 * @param summonerId The ID of the desired summoner
+	 * @return The desired summoner
+	 * @see Summoner
+	 */
 	public Summoner getSummoner(String region, long summonerId) {
 		
 		String url = getBaseURL() + region + "/v1.2/summoner/" + summonerId + "?api_key=" + getKey();
@@ -463,6 +639,12 @@ public class RiotAPI {
 	    return summoner;
 	}
 	
+	/**
+	 * Get a summoner by ID
+	 * @param summonerId The ID of the desired summoner
+	 * @return The desired summoner
+	 * @see Summoner
+	 */
 	public Summoner getSummoner(long summonerId) {
 		
 		String url = getBaseURL() + getRegion() + "/v1.2/summoner/" + summonerId + "?api_key=" + getKey();
@@ -480,6 +662,13 @@ public class RiotAPI {
 	    return summoner;
 	}
 	
+	/**
+	 * Get summoner names by IDs
+	 * @param region The desired region
+	 * @param summonerIds The IDs of the desired summoners
+	 * @return A list of desired summoner names
+	 * @see SummonerNameList
+	 */
 	public SummonerNameList getSummonerNames(String region, long... summonerIds){
 	
 		String ids = "";
@@ -503,6 +692,12 @@ public class RiotAPI {
 	    return summonerNameList;
 	}
 	
+	/**
+	 * Get summoner names by IDs
+	 * @param summonerIds The IDs of the desired summoners
+	 * @return A list of desired summoner names
+	 * @see SummonerNameList
+	 */
 	public SummonerNameList getSummonerNames(long... summonerIds){
 		
 		String ids = "";
@@ -526,6 +721,13 @@ public class RiotAPI {
 	    return summonerNameList;
 	}
 	
+	/**
+	 * Get teams of a given summoner
+	 * @param region The desired region
+	 * @param summonerId The ID of the desired summoner
+	 * @return The teams the given summoner is a member of
+	 * @see Team
+	 */
 	public List<Team> getTeams(String region, long summonerId){
 		
 		String url = getBaseURL() + region + "/v2.2/team/by-summoner/" + summonerId + "?api_key=" + getKey();
@@ -543,6 +745,12 @@ public class RiotAPI {
 	    return teamList;
 	}
 	
+	/**
+	 * Get teams of a given summoner
+	 * @param summonerId The desired summoner
+	 * @return The teams the given summoner is a member of
+	 * @see Team
+	 */
 	public List<Team> getTeams(long summonerId){
 		
 		String url = getBaseURL() + getRegion() + "/v2.2/team/by-summoner/" + summonerId + "?api_key=" + getKey();
@@ -559,7 +767,11 @@ public class RiotAPI {
 		
 	    return teamList;
 	}
-
+	
+	/**
+	 * Get the currently set season
+	 * @return The currently set season
+	 */
 	public String getSeason() {
 		if(season.equals("")) {
 			try {
@@ -571,6 +783,10 @@ public class RiotAPI {
 		return season;
 	}
 	
+	/**
+	 * Get the currently set API key
+	 * @return The currently set API key
+	 */
 	public String getKey() {
 		if(key.equals("YOUR API KEY")) {
 			try {
@@ -582,6 +798,10 @@ public class RiotAPI {
 		return key;
 	}
 	
+	/**
+	 * Get the currently set region
+	 * @return The currently set region
+	 */
 	public String getRegion() {
 		if(region.equals("")) {
 			try {
@@ -593,18 +813,34 @@ public class RiotAPI {
 		return region;
 	}
 	
+	/**
+	 * Set the season
+	 * @param season The season to set
+	 */
 	public void setSeason(String season) {
 		this.season = season;
 	}
 	
+	/**
+	 * Set the API key
+	 * @param key The API key to set
+	 */
 	public void setKey(String key) {
 		this.key = key;
 	}
 	
+	/**
+	 * Set the region
+	 * @param region The region to set
+	 */
 	public void setRegion(String region) {
 		this.region = region;
 	}
 	
+	/**
+	 * Get the base URL for all API requests
+	 * @return The base URL for all API requests
+	 */
 	public String getBaseURL() {
 		return baseURL;
 	}
