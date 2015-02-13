a19b05bb3c78c604704e23fbe3b4f19e36e822ac
Taylor Caldwell <tcaldwel@nmsu.edu>
2015-02-13 12:14:54 -0700

    Add gameQueueConfigIds to QueueType constant

--- src/constant/QueueType.java
+++ src/constant/QueueType.java
@@ -1,5 +1,7 @@
 package constant;
 
+import java.util.HashMap;
+
 /*
  * Copyright 2014 Taylor Caldwell
  *
@@ -38,7 +40,7 @@ public enum QueueType {
 		GROUP_FINDER_5x5("Team Builder"),
 		ARAM_5x5("ARAM"),
 		ONEFORALL_5x5("One for All"),
-                ONEFORALL_MIRRORMODE_5x5("One for All: Mirror Mode"),
+        ONEFORALL_MIRRORMODE_5x5("One for All: Mirror Mode"),
 		FIRSTBLOOD_1x1("Snowdown Showdown 1v1"),
 		FIRSTBLOOD_2x2("Snowdown Showdown 2v2"),
 		SR_6x6("Hexakill"),
@@ -51,7 +53,48 @@ public enum QueueType {
 		HEXAKILL("Twisted Treeline 6x6 Hexakill"),
 		KING_PORO_5x5("King Poro");
 
+		private static final HashMap<Integer, String> queueTypes;
 	    private String name;
+	    
+	    static {
+	    	HashMap<Integer, String> hm = new HashMap<Integer, String>();
+			hm.put(0, "Custom");
+			hm.put(2, "Normal 5v5 Blind Pick");
+			hm.put(7, "Historical Summoner's Rift Coop vs AI");
+			hm.put(31, "Summoner's Rift Coop vs AI Intro Bot");
+			hm.put(32, "Summoner's Rift Coop vs AI Beginner Bot");
+			hm.put(33, "Historical Summoner's Rift Coop vs AI Intermediate Bot");
+			hm.put(8, "Normal 3v3");
+			hm.put(14, "Normal 5v5 Draft Pick");
+			hm.put(16, "Dominion 5v5 Blind Pick");
+			hm.put(17, "Dominion 5v5 Draft Pick");
+			hm.put(25, "Dominion Coop vs AI");
+			hm.put(4, "Ranked Solo 5v5");
+			hm.put(9, "Ranked Premade 3v3");
+			hm.put(6, "Ranked Premade 5v5");
+			hm.put(41, "Ranked Team 3v3");
+			hm.put(42, "Ranked Team 5v5");
+			hm.put(52, "Twisted Treeline Coop vs AI");
+			hm.put(61, "Team Builder");
+			hm.put(65, "ARAM");
+			hm.put(70, "One for All");
+			hm.put(72, "Snowdown Showdown 1v1");
+			hm.put(73, "Snowdown Showdown 2v2");
+			hm.put(75, "Hexakill");
+			hm.put(76, "Ultra Rapid Fire");
+			hm.put(83, "Ultra Rapid Fire games played against AI");
+			hm.put(91, "Doom Bots Rank 1");
+			hm.put(92, "Doom Bots Rank 2");
+			hm.put(93, "Doom Bots Rank 5");
+			hm.put(96, "Ascension");
+			hm.put(98, "Twisted Treeline 6x6 Hexakill");
+			hm.put(300, "King Poro");
+	    	queueTypes = hm;
+	    }
+	    
+	    public static String getQueueNameByConfigId(int gameQueueConfigId) {
+	    	return queueTypes.get(gameQueueConfigId);
+	    }
 	    
 	    QueueType(String name) {
 	        this.name = name;
