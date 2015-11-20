fe9bd38de17789b0ec7074428bf506cbbf06aff3
Linnun <github@danielfigge.net>
2015-11-20 06:03:13 +0100

    updated version number in javadoc

--- src/main/java/riotapi/RiotApi.java
+++ src/main/java/riotapi/RiotApi.java
@@ -62,7 +62,8 @@ import dto.Team.Team;
  * Riot Games API Java Library - riot-api-java
  * 
  * @author Taylor 'rithms' Caldwell - http://rithms.net
- * @version 3.7.0
+ * @author Daniel 'Linnun' Figge
+ * @version 3.7.1
  */
 public class RiotApi {
 
