2d6fec1b3c3ff9b7c4ed6bb8b790c07dcd1ca0d9
Linnun <github@danielfigge.net>
2015-11-21 10:04:51 +0100

    clean up

--- src/util/Convert.java
+++ src/util/Convert.java
@@ -31,7 +31,6 @@ public final class Convert {
 	}
 
 	public static String[] normalizeSummonerNames(String... summonerNames) {
-		// Note for contributors: If you change this, don't strip commas! It could break comma-separated lists of summonerNames
 		return Arrays.stream(summonerNames).map(s -> normalizeSummonerName(s)).toArray(size -> new String[size]);
 	}
 }
