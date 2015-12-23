a79be2b9517075927c0719a5bc5b10301acc0ce1
Linnun <github@danielfigge.net>
2015-12-23 22:02:37 +0100

    Fixed paths for the new packages
    My IDE went totally nuts with the package changes, because the package
    name was now different than the path

---
+++ src/net/rithms/test/ConnectTest.java
@@ -0,0 +1,30 @@
+package net.rithms.test;
+
+import java.util.Map;
+
+import org.junit.Test;
+
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import net.rithms.riot.api.RiotApi;
+import net.rithms.riot.api.RiotApiException;
+import net.rithms.riot.constant.Region;
+import net.rithms.riot.dto.Summoner.Summoner;
+
+/**
+ * Created by murik on 15.12.15.
+ */
+public class ConnectTest {
+
+	@Ignore
+	@Test
+	public void testConnect() throws RiotApiException {
+
+		RiotApi api = new RiotApi("YOUR-API-KEY-HERE");
+
+		Map<String, Summoner> summoners = api.getSummonersByName(Region.NA, "rithms, tryndamere");
+		Summoner summoner = summoners.get("rithms");
+		long id = summoner.getId();
+		System.out.println(id);
+	}
+
+}
