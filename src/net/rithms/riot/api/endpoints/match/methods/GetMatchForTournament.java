0b2d574dd355e10a5ac5fa85935a89d56f210015
Taylor Caldwell <tcaldwel@nmsu.edu>
2016-01-26 09:34:04 -0700

    Add new Match API methods

---
+++ src/net/rithms/riot/api/endpoints/match/methods/GetMatchForTournament.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2016 Taylor Caldwell
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.rithms.riot.api.endpoints.match.methods;
+
+import net.rithms.riot.api.ApiConfig;
+import net.rithms.riot.api.endpoints.ApiMethod;
+import net.rithms.riot.api.endpoints.UrlParameter;
+import net.rithms.riot.api.endpoints.match.dto.MatchDetail;
+import net.rithms.riot.constant.Region;
+
+public class GetMatchForTournament extends ApiMethod {
+
+	public GetMatchForTournament(ApiConfig config, Region region, long matchId, String tournamentCode, boolean includeTimeline) {
+		super(config);
+		setDtoClass(MatchDetail.class);
+		setUrlBase(region.getEndpoint() + "/v2.2/match/for-tournament/" + matchId);
+		add(new UrlParameter("tournamentCode", tournamentCode));
+		if(includeTimeline) {
+			add(new UrlParameter("includeTimeline", includeTimeline));
+		}
+		addApiKeyParameter();
+	}
+}
