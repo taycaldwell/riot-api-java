48c8b44a9d150bf91ce75a7b46d70ae0baec958f
Linnun <github@danielfigge.net>
2016-02-11 13:18:28 +0100

    add missing licence text

--- src/net/rithms/riot/api/request/ratelimit/RateLimitList.java
+++ src/net/rithms/riot/api/request/ratelimit/RateLimitList.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2016 Taylor Caldwell
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
 package net.rithms.riot.api.request.ratelimit;
 
 import java.util.Map;
@@ -5,6 +18,11 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import net.rithms.riot.constant.Region;
 
+/**
+ * This class provides a list of rate limits and retry times.
+ * 
+ * @author Daniel 'Linnun' Figge
+ */
 public class RateLimitList {
 
 	private final Map<Region, RateLimit> userLimits = new ConcurrentHashMap<Region, RateLimit>();
